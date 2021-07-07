f3481d6ca2f43e8b9c2f71a299510b7efddbbb16
YUYA TABUCHI <glagladodo17@gmail.com>
2021-07-07 20:42:30 +0900

    8th commit

--- resources/views/Youtune/layout.blade.php
+++ resources/views/Youtune/layout.blade.php
@@ -11,7 +11,7 @@
   <link rel="stylesheet" href="css/jquery.range.css">
   <script src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script src="js/jquery.range.js"></script>
+  <script src="../../js/jquery.range.js"></script>
   <!-- <script>
   $('.single-slider').jRange({
     from: 1,
