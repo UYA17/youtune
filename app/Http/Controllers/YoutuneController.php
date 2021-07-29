46ceef52d9b8af72b424e87b706c9167484d4060
YUYA TABUCHI <glagladodo17@gmail.com>
2021-07-29 12:39:24 +0900

    11th commit

--- app/Http/Controllers/YoutuneController.php
+++ app/Http/Controllers/YoutuneController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Vender\CallYoutubeApi;
 use Google_Client;
 use Google_Service_YouTube;
 
@@ -17,9 +18,17 @@ class YoutuneController extends Controller
     {
         return view('Youtune.choice');
     }
-    public function search()
+    public function search(Request $request)
     {
-        return view('Youtune.search');
+        $t = new CallYoutubeApi();
+        $searchList = $t->searchList("スピッツ");
+        foreach ($searchList as $result) {
+            $videosList = $t->videosList($result->id->video);
+            $embed = "https//www.youtube.com/embed/" . $videosList[0]['id'];
+            $array[] = array($embed, $videosList[0]['snippet'], $videosList[0]['statistics']);
+        }
+
+        return view('Youtune.search', ['Youtune.search' => $array]);
     }
     public function mypage()
     {
---
+++ app/Http/Vender/CallYoutubeApi.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Vender;
+
+use Google_Client;
+use Google_Service;
+use Google_Service_YouTube;
+
+class CallYoutubeApi
+{
+  private $key = 'AIzaSyDya9tFGu5q7vw1g-ZeEq6Z54ub147yigk';
+  private $client;
+  private $youtube;
+
+  public function __construct()
+  {
+    $this->client = new Google_Client();
+    $this->client->setDeveloperKey($this->key);
+    $this->youtube = new Google_Service_YouTube($this->client);
+  }
+
+  /**
+   * /v3/searchを呼び出す
+   *
+   * @param string $searchWord
+   * @return array
+   */
+  public function searchList(String $searchWord)
+  {
+    $r = $this->youtube->search->listSearch('id', array(
+      'q' => $searchWord,
+      'maxResults' => 10,
+      'order' => 'viewCount'
+    ));
+    return $r->items;
+  }
+  /**
+   * /v3/videosを呼び出す
+   *
+   * @param string $id
+   * @return array
+   */
+  public function videosList(String $id)
+  {
+    $r = $this->youtube->videos->listVideos('statistics,snippet', array(
+      'id' => $id,
+    ));
+
+    return $r->items;
+  }
+}
